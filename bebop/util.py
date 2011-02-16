1e284e034e5d49435796cc2c54a742e11ef512a1
al@ubuntu.ubuntu-domain <al@ubuntu.ubuntu-domain>
2011-02-16 17:12:56 -0500

    [fix] Adding support for specifying child_order for schema elements where needed (otherwise it just iterates through the object's dict, unordered)

--- bebop/util.py
+++ bebop/util.py
@@ -7,8 +7,9 @@ Created on Jan 19, 2011
 from lxml import etree
 from lxml import builder
 from lxml import objectify
+import collections
 import os
-
+        
 E = objectify.ElementMaker(annotate=False)
 
 def ensure_dir(d):
@@ -58,7 +59,14 @@ def _to_xml(obj):
     for attr, transformed in obj.required.iteritems():
         definitions[transformed] = _stringify(getattr(obj, attr))
             
-    for attr, value in obj.__dict__.iteritems():
+    q = OrderedSet()
+    # Prioritize elements if specified in an iterable called child_order
+    if hasattr(obj, 'child_order'):
+        [q.add(k) for k in obj.child_order]
+    [q.add(k) for k in obj.__dict__]
+         
+    for attr in q:
+        value = getattr(obj, attr)
         # Append children first (recursive, but we don't need many levels)
         # TODO: order this correctly, still using an unsorted dict
         if hasattr(value, 'to_xml'):
@@ -90,7 +98,7 @@ class BaseSolrXMLElement(object):
         for k, v in kw.iteritems():
             setattr(self, k, v)
             
-        # So class.to_xml will return the 
+        # So class.to_xml will call _to_xml(cls) and self.to_xml will call _to_xml(self)
         self.to_xml = self.instance_to_xml
     
     def instance_to_xml(self):
@@ -106,3 +114,65 @@ class BaseSolrXMLElement(object):
         return dict([(attr, _to_camelcase(attr))
                      for attr in cls.options])    
 
+
+KEY, PREV, NEXT = range(3)
+
+class OrderedSet(collections.MutableSet):
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[PREV]
+            curr[NEXT] = end[PREV] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, next = self.map.pop(key)
+            prev[NEXT] = next
+            next[PREV] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[NEXT]
+        while curr is not end:
+            yield curr[KEY]
+            curr = curr[NEXT]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[PREV]
+        while curr is not end:
+            yield curr[KEY]
+            curr = curr[PREV]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = next(reversed(self)) if last else next(iter(self))
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
+    def __del__(self):
+        self.clear()                    # remove circular references
