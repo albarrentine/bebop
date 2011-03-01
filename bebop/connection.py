d39c9cebd5d2972098f00e78bdf6d07422635307
Al <al@jumo.com>
2011-03-01 02:00:40 -0500

    [connection] Adding some pysolr methods

--- bebop/connection.py
+++ bebop/connection.py
@@ -8,7 +8,7 @@ import pysolr
 
 _solr_conns = {}
 
-class Solr(object):
+class Bebop(object):
     def __init__(self, host, port, solr_dir='solr', id='main'):   
         self._solr = _solr_conns[id] = _solr_conns.get(id) or pysolr.Solr('http://%s:%s/%s/' % (host, port, solr_dir))
       
@@ -19,8 +19,17 @@ class Solr(object):
     def search(self, query, id='main'):
         return self._solr.search(query)
     
-    def add(self):
-        pass
+    def add(self, doc, commit=True):
+        self._solr.add(doc._to_solr_doc(),commit=commit)
 
-    def update(self):
-        pass
+    def batch_add(self, docs, commit=False):
+        self._solr.add([doc._to_solr_doc() for doc in docs], commit=commit)
+        
+    def commit(self):
+        self._solr.commit()
+        
+    def optimize(self):
+        self._solr.optimize()
+        
+    def rollback(self):
+        self._solr.rollback()
