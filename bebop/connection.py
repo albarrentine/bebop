a960b19f3f88edc9e82bf5d8868f7d9768f98f8d
Al <al@jumo.com>
2011-06-20 17:27:37 -0400

    [rename] Renaming some of the connection stuff

--- bebop/connection.py
+++ bebop/connection.py
@@ -4,32 +4,36 @@ Created on Feb 18, 2011
 @author: al
 '''
 
+#@todo: rewrite pysolr
 import pysolr
+try:
+    import gevent
+    gevent.monkey.patch_all()
+except ImportError:
+    try:
+        import eventlet
+        eventlet.monkey_patch()
+    except ImportError:
+        pass
 
-_solr_conns = {}
+class SolrConn(object):
+    def __init__(self, conn, id='main'):
+        self._solr = pysolr.Solr(conn)
 
-class Bebop(object):
-    def __init__(self, host, port, solr_dir='solr', id='main'):   
-        self._solr = _solr_conns[id] = _solr_conns.get(id) or pysolr.Solr('http://%s:%s/%s/' % (host, port, solr_dir))
-      
-    @property
-    def raw_conn(self):
-        return _solr_conns[self.id]
-              
     def search(self, query, id='main'):
-        return self._solr.search(query)
-    
+        return self._solr.search(**query.params)
+
     def add(self, doc, commit=True):
         self._solr.add(doc._to_solr_doc(),commit=commit)
 
     def batch_add(self, docs, commit=False):
         self._solr.add([doc._to_solr_doc() for doc in docs], commit=commit)
-        
+
     def commit(self):
         self._solr.commit()
-        
+
     def optimize(self):
         self._solr.optimize()
-        
+
     def rollback(self):
         self._solr.rollback()
