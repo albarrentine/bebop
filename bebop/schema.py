6a3e7fafdfed5ac6d95b006bb854a4f28fc08015
Al <al@jumo.com>
2011-06-20 18:49:35 -0400

    [move] Moving Field around again

--- bebop/schema.py
+++ bebop/schema.py
@@ -19,7 +19,7 @@ class SolrFieldType(BaseSolrXMLElement):
                'position_increment_gap'
                ]
 
-class String(SolrFieldType): 
+class String(SolrFieldType):
     solr_class='solr.StrField'
     type_name='string'
     omit_norms = True
@@ -27,7 +27,7 @@ class String(SolrFieldType):
 
 class UniqueId(SolrFieldType):
     solr_class='solr.UUIDField'
-    type_name='uuid'    
+    type_name='uuid'
 
 class Boolean(SolrFieldType):
     solr_class = 'solr.BoolField'
@@ -38,35 +38,35 @@ class Boolean(SolrFieldType):
 class Date(SolrFieldType):
     solr_class = 'solr.DateField'
     type_name = 'date'
-    
+
 class Random(SolrFieldType):
     solr_class = 'solr.RandomSortField'
     type_name = 'random'
 
-""" 
-Numeric types 
+"""
+Numeric types
 """
 
 class Integer(SolrFieldType):
     solr_class = 'solr.IntField'
     type_name = 'int'
     omit_norms = True
-    
+
 class Long(SolrFieldType):
     solr_class = 'solr.LongField'
     type_name = 'long'
     omit_norms = True
-    
+
 class Float(SolrFieldType):
     solr_class = 'solr.FloatField'
     type_name = 'float'
     omit_norms = True
-    
+
 class Double(SolrFieldType):
     solr_class = 'solr.DoubleField'
     type_name = 'double'
     omit_norms = True
-    
+
 class SortableInteger(SolrFieldType):
     solr_class = 'solr.SortableIntField'
     type_name = 'sint'
@@ -78,7 +78,7 @@ class SortableLong(SolrFieldType):
     type_name = 'slong'
     omit_norms = True
     sort_missing_last = True
-    
+
 class SortableDouble(SolrFieldType):
     solr_class = 'solr.SortableDoubleField'
     type_name = 'sdouble'
@@ -94,144 +94,144 @@ class SortableFloat(SolrFieldType):
 """
 Tokenizers
 """
-    
+
 class Tokenizer(BaseSolrXMLElement):
     solr_class = None
     tag = "tokenizer"
     required = {'solr_class': 'class'}
-    
+
 class WhitespaceTokenizer(Tokenizer):
     solr_class = 'solr.WhitespaceTokenizerFactory'
-       
+
 class StandardTokenizer(Tokenizer):
     solr_class = 'solr.StandardTokenizerFactory'
-    
+
 class KeywordTokenizer(Tokenizer):
     solr_class = 'solr.KeywordTokenizerFactory'
-    
+
 class LetterTokenizer(Tokenizer):
     solr_class = 'solr.LetterTokenizerFactory'
-    
+
 class HTMLStripWhitespaceTokenizer(Tokenizer):
     solr_class = 'solr.HTMLStripWhitespaceTokenizerFactory'
-    
+
 class HTMLStripStandardTokenizer(Tokenizer):
     solr_class = 'solr.HTMLStripStandardTokenizerFactory'
-    
+
 class PatternTokenizer(Tokenizer):
     """ To be subclassed further as needed, for instance:
-    
+
     class GroupingPatternTokenizer(PatternTokenizer):
         pattern= "\'([^\']+)\'"
         group = 1
-        
+
     """
     solr_class = 'solr.PatternTokenizerFactory'
     options = ['pattern', 'group']
-        
+
 """
 Filter classes
 """
-        
+
 class Filter(BaseSolrXMLElement):
     solr_class = None
     tag = "filter"
     options = []
     required = {'solr_class': 'class'}
-    
+
 class LowerCaseFilter(Filter):
     solr_class = "solr.LowerCaseFilterFactory"
-    
+
 class StandardFilter(Filter):
     solr_class = 'solr.StandardFilterFactory'
-    
+
 class TrimFilter(Filter):
     solr_class = 'solr.TrimFilterFactory'
     options = ['update_offsets']
-    
+
 class LengthFilter(Filter):
     solr_class = 'solr.LengthFilterFactory'
     options = ['min', 'max']
-    
+
 class RemoveDuplicatesFilter(Filter):
     """ Add this as your last analysis step"""
     solr_class = 'solr.RemoveDuplicatesTokenFilterFactory'
-    
+
 class ISOLatin1AccentFilter(Filter):
     """ Normalize accented characters """
     solr_class = 'solr.ISOLatin1AccentFilterFactory'
-    
+
 class CapitalizationFilter(Filter):
     solr_class = 'solr.CapitalizationFilterFactory'
 
 class PatternReplaceFilter(Filter):
     """ To be subclassed further as needed"""
     solr_class = 'solr.PatternReplaceFilterFactory'
-    options = ['pattern', 'replacement', 'replace']    
-    
-""" 
+    options = ['pattern', 'replacement', 'replace']
+
+"""
 CharFilterFactories are available only for Solr 1.4 and higher
 """
 
 class MappingCharFilter(Filter):
     solr_class = 'solr.MappingCharFilterFactory'
-    
+
 class PatternReplaceCharFilter(Filter):
     """ To be subclassed further as needed"""
     solr_class = 'solr.PatternReplaceCharFilterFactory'
     options = ['pattern', 'replacement', 'replace']
-    
+
 class HTMLStripCharFilter(Filter):
     """ Strips out HTML embedded in text. Good for blog posts, etc.
     that are not HTML documents themselves but may contain HTML """
     solr_class = 'solr.HTMLStripCharFilterFactory'
-    
+
 """
 WordDelimiter - highly customizable, don't just use mine
 """
-    
+
 class WordDelimiterFilter(Filter):
     solr_class = 'solr.WordDelimiterFilterFactory'
     options = ['generate_word_parts',
                 'generate_number_parts', 'catenate_words',
-                'catenate_numbers', 'catenate_all', 
+                'catenate_numbers', 'catenate_all',
                 'split_on_case_change', 'split_on_numerics',
                 'stem_english_possessive', 'preserve_original']
-        
+
 class EnglishPorterFilter(Filter):
     solr_class = 'solr.EnglishPorterFilterFactory'
     options = ['protected']
-    
+
 class SnowballPorterFilter(Filter):
     solr_class = 'solr.SnowballPorterFilterFactory'
     options = ['language']
-    
+
 class SynonymFilter(Filter):
     solr_class = 'solr.SynonymFilterFactory'
     options = ['synonyms', 'ignore_case', 'expand']
-    
+
 class StopFilter(Filter):
     solr_class = 'solr.StopFilterFactory'
     options = ['words', 'ignore_case']
-    
+
 """
 Phonetic filters for sounds-like analysis
 """
-    
+
 class DoubleMetaphoneFilter(Filter):
     solr_class = 'solr.DoubleMetaphoneFilterFactory'
     options = ['inject', 'max_code_length']
 
 class PhoneticFilter(Filter):
     solr_class = 'solr.PhoneticFilterFactory'
-    options = ['encoder', 'inject']    
-    
+    options = ['encoder', 'inject']
+
 class MetaphoneFilter(PhoneticFilter):
     encoder = 'Metaphone'
 
 class SoundexFilter(PhoneticFilter):
     encoder = 'Soundex'
-    
+
 class RefinedSoundexFilter(PhoneticFilter):
     encoder = 'RefinedSoundex'
 
@@ -245,22 +245,22 @@ class NGramFilter(Filter):
     options = ['min_gram_size', 'max_gram_size']
     min_gram_size = 2
     max_gram_size = 15
-   
+
 class EdgeNGramFilter(NGramFilter):
     solr_class = 'solr.EdgeNGramFilterFactory'
     options = NGramFilter.options + ['side']
-    
-""" 
+
+"""
 Analyzer
 """
-    
+
 class Analyzer(BaseSolrXMLElement):
     INDEX = 'index'
     QUERY = 'query'
-    
+
     tag = 'analyzer'
     options = ['type']
-    
+
     tokenizer = WhitespaceTokenizer
     filters = ()
 
@@ -272,18 +272,18 @@ All text-based field_types
 class BaseText(SolrFieldType):
     solr_class = 'solr.TextField'
     position_increment_gap = 100
-    
+
 class SimpleTokenizedText(BaseText):
     type_name = 'text_ws'
     anaylzer = Analyzer(tokenizer=WhitespaceTokenizer)
 
 class Text(BaseText):
     type_name = 'text'
-    
+
     index_analyzer = Analyzer(type=Analyzer.INDEX,
                               tokenizer = WhitespaceTokenizer,
                               filters = (StopFilter(ignore_case=True, words='stopwords.txt', enable_position_increments=True),
-                                         WordDelimiterFilter(generate_word_parts=1, generate_number_parts=1, catenate_words=1, 
+                                         WordDelimiterFilter(generate_word_parts=1, generate_number_parts=1, catenate_words=1,
                                                              catenate_numbers=1, catenate_all=0, split_on_case_change=1),
                                          LowerCaseFilter,
                                          ISOLatin1AccentFilter,
@@ -295,18 +295,18 @@ class Text(BaseText):
                               tokenizer = WhitespaceTokenizer,
                               filters = (SynonymFilter(synonyms='synonyms.txt', ignore_case=True, expand=True),
                                          StopFilter(ignore_case=True, words='stopwords.txt'),
-                                         WordDelimiterFilter(generate_word_parts=1, generate_number_parts=1, catenate_words=1, 
+                                         WordDelimiterFilter(generate_word_parts=1, generate_number_parts=1, catenate_words=1,
                                                              catenate_numbers=0, catenate_all=0, split_on_case_change=1),
                                          LowerCaseFilter,
                                          ISOLatin1AccentFilter,
                                          EnglishPorterFilter(protected='protwords.txt'),
                                          RemoveDuplicatesFilter
                                          )
-                              
+
                               )
 class TextTight(BaseText):
     type_name = 'textTight'
-    
+
     analyzer = Analyzer(tokenizer = WhitespaceTokenizer,
                         filters = (SynonymFilter(synonyms='synonyms.txt', ignore_case=True, expand=False),
                                    StopFilter(ignore_case=True, words='stopwords.txt'),
@@ -319,7 +319,7 @@ class TextTight(BaseText):
 
 class Title(BaseText):
     type_name = 'title'
-    
+
     analyzer = Analyzer(tokenizer = WhitespaceTokenizer,
                         filters = (StopFilter(ignore_case=True, words='stopwords.txt'),
                                    WordDelimiterFilter(generate_word_parts=0, generate_number_parts=0, catenate_words=1, catenate_numbers=1, catenate_all=0),
@@ -333,7 +333,7 @@ class TextSpell(BaseText):
     type_name = 'textSpell'
     stored = False
     multi_valued = True
-    
+
     index_analyzer = Analyzer(type=Analyzer.INDEX,
                               tokenizer = StandardTokenizer,
                               filters = (LowerCaseFilter,
@@ -364,7 +364,7 @@ class AlphaOnlySort(BaseText):
     type_name = 'alphaOnlySort'
     omit_norms = True
     sort_missing_last = True
-    
+
     analyzer = Analyzer(tokenizer = KeywordTokenizer,
                         filters = (LowerCaseFilter,
                                    TrimFilter,
@@ -386,10 +386,10 @@ class SolrFieldTypes(BaseSolrXMLElement):
              Integer, Long, Float, Double,
              SortableInteger, SortableLong,
              SortableFloat, SortableDouble,
-             Date, Random, SimpleTokenizedText, 
+             Date, Random, SimpleTokenizedText,
              Text, TextTight, Title, TextSpell,
-             TextSpellPhrase, AlphaOnlySort, Ignored)    
-    
+             TextSpellPhrase, AlphaOnlySort, Ignored)
+
     def __init__(self, *args):
         self.types = tuple(args)
 
@@ -405,11 +405,11 @@ class SolrSchemaField(BaseSolrXMLElement):
         super(SolrSchemaField, self).__init__(name=name, type=type.type_name)
 
 
-class SolrSchemaFields(BaseSolrXMLElement):  
+class SolrSchemaFields(BaseSolrXMLElement):
     tag = 'fields'
     fields = ()
     dependency = SolrFieldTypes
-    
+
     def __init__(self, *args):
         super(SolrSchemaFields, self).__init__(fields=args)
 
@@ -437,8 +437,9 @@ class SolrSchema(BaseSolrXMLElement):
         self.fields = fields
         super(SolrSchema, self).__init__(**kw)
 
+
 def generate_schema(schema, path = 'solr/conf/schema.xml'):
     ensure_dir(os.path.dirname(path))
-    
+
     tree = etree.ElementTree(schema.to_xml())
-    tree.write(path, encoding='utf-8', xml_declaration=True, pretty_print=True)                    
+    tree.write(path, encoding='utf-8', xml_declaration=True, pretty_print=True)
