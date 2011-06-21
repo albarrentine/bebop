5598f885990e02656fc27202e5554b1a0620b220
Al <al@jumo.com>
2011-06-21 12:15:40 -0400

    [fix] Commenting out SolrFunctions thing for now, just an experiment, coming soon

--- bebop/__init__.py
+++ bebop/__init__.py
@@ -1,11 +1,13 @@
 from schema import *
+from index import *
 from model import *
+from query import *
 from config import *
 from data_import import *
 from interface import *
 
-class _SolrFunctions(object):
-    def __getattr__(self, attr):
-        pass
+#class _SolrFunctions(object):
+#    def __getattr__(self, attr):
+#        pass
 
-func = _SolrFunctions()
+#func = _SolrFunctions()
