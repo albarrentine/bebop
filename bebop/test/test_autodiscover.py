695afb44b67d08189d48a0c87ba04874ff3f4a43
Al <al@jumo.com>
2011-06-21 12:14:46 -0400

    [tests] Updating tests to use new API

--- bebop/test/test_autodiscover.py
+++ bebop/test/test_autodiscover.py
@@ -8,10 +8,10 @@ from bebop import Solr
 from unittest import TestCase
 
 class TestModel(TestCase):
-        
+
     def test_autodiscover(self):
         import bebop.test
         solr = Solr()
-        indexes = solr.autodiscover_indexes(bebop.test)
-        solr.generate_solr_configs(indexes)
+        solr.autodiscover_indexes(bebop.test)
+        solr.generate_solr_configs()
         self.assertTrue(True)
--- bebop/test/test_model.py
+++ bebop/test/test_model.py
@@ -5,6 +5,7 @@ Created on Feb 14, 2011
 '''
 
 from bebop import *
+import bebop.test
 from unittest import TestCase
 
 class FooDB(object):
@@ -31,7 +32,7 @@ class TestModel(TestCase):
 
     def test_internals(self):
         self.assertEquals(Foo.__solr_index__, 'foo')
-        self.assertEquals(Foo._fields, ['id', 'name'])
+        self.assertEquals(Foo._solr_fields, ['id', 'name'])
 
     def test_equals(self):
         clause = Foo.name == 'blah'
@@ -40,3 +41,9 @@ class TestModel(TestCase):
     def test_boolean_clause(self):
         clause = and_(Foo.id > 5, or_(Foo.name=='blah', Foo.name=='blee'))
         self.assertEquals(clause, "(id:[5 TO *] AND (name:blah OR name:blee))")
+
+    def test_search_url(self):
+        solr = Solr()
+        solr.autodiscover_indexes(bebop.test)
+        q = solr.search(Bar).query('baz').filter(Bar.id>10).fields(Bar.name).limit(10).offset(5)
+        self.assertEquals(q.params.items(), [('q', 'baz'),('fq', 'id:[10 TO *]'),('fl', ['name']),('rows',10),('start',5)] )
