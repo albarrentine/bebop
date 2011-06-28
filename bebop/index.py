02304c55734c089693229462538961747d8b55d9
Al <al@jumo.com>
2011-06-28 18:14:32 -0400

    [indexes] Adding a default constructor to indexes IF one has not already been provided, changing method name from _to_solr_doc to _to_dict

--- bebop/index.py
+++ bebop/index.py
@@ -8,12 +8,25 @@ from schema import SolrSchema, SolrSchemaFields, SolrFieldTypes
 from model import Field
 from config import StandardSolrConfig
 
+def has_own_constructor(cls):
+    # There's also this awesome black magic-ish statement:
+    # sum([cls.__init__ == base.__init__ for base in cls.__mro__[1:]]) == 0
+    # but I'm not going to mess around with people's inheritance
+    return hasattr(cls, '__init__') and cls.__init__ != object.__init__
+
 def SearchIndex(name, config=StandardSolrConfig, generate_schema=True):
-    def _to_solr_doc(self):
+    def _to_dict(self):
         return dict([(v, getattr(self, k)) for k, v in self._models_to_solr.iteritems()])
 
     def _create_target_model(self):
         return self._target(**dict([(k, v) for k,v in self._solr_to_models.iteritems()]))
+
+    def default_constructor(self, obj=None, **kw):
+        arg_dict = obj if hasattr(obj, 'iteritems') else obj.__dict__
+        arg_dict.update(kw)
+        for attr, value in arg_dict.iteritems():
+            if attr in self._solr_fields:
+                setattr(self, attr, value)
 
     def _Index(cls):
         cls.__solr_index__ = name
@@ -38,7 +51,10 @@ def SearchIndex(name, config=StandardSolrConfig, generate_schema=True):
                                   )
         cls.solr_config=config
 
-        cls._to_solr_doc = _to_solr_doc
+        cls._to_dict = _to_dict
         cls._create_target_model = _create_target_model
+
+        if not has_own_constructor(cls) :
+            cls.__init__ = default_constructor
         return cls
     return _Index
