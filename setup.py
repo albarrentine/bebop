3841732c30af8ca5d89766b42f8a1c2d14ea8660
Al <al@jumo.com>
2011-06-29 17:31:14 -0400

    Changing description

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from distutils.core import setup
 setup(
     name='bebop',
     version='0.0.1dev',
-    description='Solr abstraction library',
+    description='Lightweight Solr object mapping and schema generation without all the XML',
     author='Al Barrentine',
-    author_email='al@jumo.com',
     url='http://www.github.com/thatdatabaseguy/bebop',
     install_requires=[
         "pysolr",
