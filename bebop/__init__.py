de4e7a6d4089243e162aaab8afdf4a70e1ce52e4
Al <al@jumo.com>
2011-07-03 14:16:37 -0400

    [fix] Taking the function stuff out of the __init__.py

--- bebop/__init__.py
+++ bebop/__init__.py
@@ -5,9 +5,3 @@ from query import *
 from config import *
 from data_import import *
 from interface import *
-
-#class _SolrFunctions(object):
-#    def __getattr__(self, attr):
-#        pass
-
-#func = _SolrFunctions()
