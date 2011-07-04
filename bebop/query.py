363f0efba3c62f2ab81563f244baae8759b7d5e6
Al <al@jumo.com>
2011-07-03 23:29:40 -0400

    [query] Adding faceting, a wrapper for pysolr results to fetch some of the other stats such as hits back from the query while still mapping rows properly, added asc and desc functions for sorts

--- bebop/query.py
+++ bebop/query.py
@@ -6,6 +6,7 @@ Created on Jun 16, 2011
 
 from schema import SolrSchemaField, UniqueKey
 from util import NotGiven, MultiDict
+import itertools
 
 def join_with_separator(separators, param, separator):
     separators[param] = separator
@@ -21,6 +22,9 @@ class SolrFunction(object):
 
     def __unicode__(self):
         return ''.join([unicode(self.name), '(', ', '.join([unicode(arg) for arg in self.args]), ')'])
+
+    def __pow__(self, power):
+        return ''.join([unicode(self), '^', power])
 
     def __call__(self, *args, **kwargs):
         self.args = args
@@ -54,39 +58,84 @@ class _SolrFunctionGenerator(object):
 func = _SolrFunctionGenerator()
 
 class SolrResult(object):
-    pass
+    """ Currently just a wrapper for pysolr result """
+    def __init__(self, res, handler):
+        self.docs = res.docs
+        self.hits = res.hits
+        self.highlighting = res.highlighting
+        self.facets = res.facets
+        self.spellcheck = res.spellcheck
+        self.stats = res.stats
+
+        self.handle_row = handler
+
+    def __iter__(self):
+        return (self.handle_row(doc) for doc in self.docs)
 
 class LuceneQuery(object):
     MANDATORY = 'mandatory'
     PROHIBITED = 'prohibited'
     OPTIONAL = 'optional'
 
-    def __init__(self, field=NotGiven, *args, **kw):
-        if field is NotGiven:
-            word, args = args[0], args[1:]
+    required_modifiers = {MANDATORY: '+',
+                          PROHIBITED: '-',
+                          OPTIONAL: ''}
+
+    def __init__(self, *args, **kw):
+        first_arg = args[0]
+        if hasattr(first_arg, 'solr_field_name'):
+            self.field, args = first_arg, args[1:]
+        elif isinstance(first_arg, LuceneQuery):
+            # Only pass LuceneQuery if all the other args are LuceneQueries
+            self.field = None
+            self.components = itertools.chain([query.components for query in args])
         else:
-            self.field = field
-        self.components = args
+            self.field = None
+
+        self.components = list(args)
+        self.local_params = MultiDict()
         self.required = LuceneQuery.OPTIONAL
+        self.boost_factor = None
 
     def __pow__(self, power):
-        self.components.extend(['^', power])
+        self.boost_factor = power
+        return self
+
+    def tag(self, tag):
+        self.local_params.add('tag', tag)
         return self
 
     def require(self):
         self.required = LuceneQuery.MANDATORY
+        return self
 
     def negate(self):
         self.required = LuceneQuery.PROHIBITED
+        return self
 
     def fuzzy(self, factor=NotGiven):
         self.components.append('~')
         if factor != NotGiven:
             self.components.append(factor)
+        return self
 
     def __unicode__(self):
-        return u''.join([unicode(self.field), u':'] + [unicode(component) for component in self.components])
+        modifier = self.required_modifiers[self.required]
+        field_clause = '' if not self.field else unicode(self.field) + ':'
+        local_params = ''
+        if self.local_params:
+            local_params = '{!' + ' '.join(['%s=%s' % (key, unicode(val)) for key, val in self.local_params.iteritems() ]) + '}'
+        component_clause = ''.join([unicode(component) for component in self.components])
+        boost_factor = self.boost_factor or ''
+        return u''.join([local_params, modifier, field_clause, component_clause, boost_factor])
 
+Q = LuceneQuery
+
+def asc(*fields):
+    return ','.join([unicode(field) + '+asc' for field in fields])
+
+def desc(*fields):
+    return ','.join([unicode(field) + '+desc' for field in fields])
 
 class SolrQuery(object):
     separators = {}
@@ -104,21 +153,65 @@ class SolrQuery(object):
     def handle_row(self, row):
         return self.index(row)
 
+    def join_params(self):
+        return MultiDict((k,self.separators[k].join([unicode(v) for v in val]) if k in self.separators else unicode(val)) for k,val in self.params.iteritems())
+
     def execute(self, handler=handle_row, conn='main'):
         if not self.params_joined:
-            self.params=MultiDict((k,self.separators[k].join([unicode(v) for v in val]) if k in self.separators else unicode(val)) for k,val in self.params.iteritems())
+            self.params=self.join_params()
             self.params_joined = True
-        return [handler(self, obj) for obj in self._execute_search()]
+        return SolrResult(self._execute_search(), handler=handler)
+
 
     def query(self, query):
-        self.params.update(q=unicode(query))
+        self.params.add('q', query)
         return self
 
-    def filter(self, filters):
-        if hasattr(filters, '__iter__'):
-            [self.params.update(fq=unicode(filter)) for filter in filters]
-        else:
-            self.params.update(fq=unicode(filters))
+    def filter(self, *filters):
+        [self.params.add('fq', filter) for filter in filters]
+        return self
+
+    def facet(self, field, method=NotGiven, missing_facet=NotGiven, prefix=NotGiven):
+        self.params['facet'] = True
+        field_name = SolrQuery._name_or_val(field)
+        self.params.add('facet.field', field_name)
+        if method != NotGiven:
+            self.params['f.%s.facet.method' % field_name] = method
+        if missing_facet != NotGiven:
+            self.params['f.%s.facet.missing' % field_name] = missing_facet
+        if prefix != NotGiven:
+            self.params['facet.prefix'] = prefix
+        return self
+
+    def facet_date(self, date_field, start_date=NotGiven, end_date=NotGiven,
+                   gap=NotGiven, hard_end=NotGiven, other=NotGiven):
+        field_name = SolrQuery._name_or_val(date_field)
+        self.params.add('facet.date', field_name)
+
+        if start_date != NotGiven:
+            self.params['f.%s.facet.date.start' % field_name] = start_date
+
+        if end_date != NotGiven:
+            self.params['f.%s.facet.date.end' % field_name] = end_date
+
+        if gap != NotGiven:
+            self.params['f.%s.facet.date.gap' % field_name] = gap
+
+        if hard_end != NotGiven:
+            self.params['f.%s.facet.date.hardend' % field_name] = hard_end
+
+        if other != NotGiven:
+            self.params['f.%s.facet.date.other' % field_name] = other
+
+        return self
+
+    def facet_query(self, query):
+        self.params.add('facet.query', query)
+        return self
+
+    def facet_prefix(self, field, prefix):
+        self.facet(field)
+        self.params['facet.prefix'] = prefix
         return self
 
     def default_operator(self, op):
@@ -137,8 +230,17 @@ class SolrQuery(object):
         self.params['tie'] = tie
         return self
 
-    def query_type(self, qt):
-        self.params['qt'] = qt
+    def default_search(self, query):
+        self.params['q.alt'] = query
+        return self
+
+    @join_with_separator(separators, 'mm', ' ')
+    def min_should_match(self, *args):
+        self._add_items_to_key('mm', *args)
+        return self
+
+    def query_type(self, query_type):
+        self.params['qt'] = query_type
         return self
 
     def paginate(self, limit, offset):
@@ -172,8 +274,8 @@ class SolrQuery(object):
         self.params['ps'] = slop
         return self
 
-    def boost_query(self, q):
-        self.params.update(bq=unicode(q))
+    def boost_query(self, query):
+        self.params.add('bq', query)
 
     @staticmethod
     def _name_or_val(arg):
@@ -222,13 +324,6 @@ class SolrQuery(object):
     def echo_params(self, echo):
         self.params['echoParams'] = echo
         return self
-
-
-    #def response_format(self, format):
-    #    self.params['wt'] = format
-
-    #def target_model(self, model, proxy):
-    #    pass
 
     def _execute_search(self):
         return self.conn.search(self)
