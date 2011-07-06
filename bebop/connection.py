ab54e579dcdebd005dc9ef5a64372ee419a11945
Al <al@jumo.com>
2011-07-05 20:13:04 -0400

    [connection] Hacking pysolr to use a MultiDict, which reeeeally is how url encoding should work

--- bebop/connection.py
+++ bebop/connection.py
@@ -21,7 +21,24 @@ class SolrConn(object):
         self._solr = pysolr.Solr(conn)
 
     def search(self, query, id='main'):
-        return self._solr.search(**query.params)
+        response = self._solr._select(query.params)
+        result = self._solr.decoder.decode(response)
+        result_kwargs = {}
+
+        if result.get('highlighting'):
+            result_kwargs['highlighting'] = result['highlighting']
+
+        if result.get('facet_counts'):
+            result_kwargs['facets'] = result['facet_counts']
+
+        if result.get('spellcheck'):
+            result_kwargs['spellcheck'] = result['spellcheck']
+
+        if result.get('stats'):
+            result_kwargs['stats'] = result['stats']
+
+        self._solr.log.debug("Found '%s' search results." % result['response']['numFound'])
+        return pysolr.Results(result['response']['docs'], result['response']['numFound'], **result_kwargs)
 
     def add(self, doc, commit=True):
         self._solr.add(doc._to_dict(),commit=commit)
